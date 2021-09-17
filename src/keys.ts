ee6b13350a20e5c359627d278c2d01ad380f78c6
unknown <pgcuascotac@utn.edu.ec>
2021-09-16 20:04:57 -0500

    probando2

--- src/keys.ts
+++ src/keys.ts
@@ -3,7 +3,8 @@ export default{
         host: 'db-prueba-do-user-9789489-0.b.db.ondigitalocean.com',
         user:  'stem',
         password:'fqKV-E_mb2q2tpSK',
-        database: 'bdd_stem'
+        database: 'bdd_stem',
+        port:'25060'
           
     }
 
