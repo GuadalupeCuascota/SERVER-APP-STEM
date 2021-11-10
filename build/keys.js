509615bfadcc34b6406ffeaff84a192c5d9434c8
unknown <pgcuascotac@utn.edu.ec>
2021-11-10 10:43:22 -0500

    bdd-stem

--- build/keys.js
+++ build/keys.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: "164.90.187.157",
+        host: "147.182.232.156",
         user: "stem",
         password: "35Love41_",
         database: "bdd_stem",
--- src/keys.ts
+++ src/keys.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 export default {
   database: {
-    host: "164.90.187.157",
+    host: "147.182.232.156",
     user: "stem",
     password: "35Love41_",
     database: "bdd_stem",
