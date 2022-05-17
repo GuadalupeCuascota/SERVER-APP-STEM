670d9cef72b9b3b1808e1dc0ae01b146b26bc4a2
unknown <pgcuascotac@utn.edu.ec>
2022-05-17 11:18:35 -0500

    subiendo al server nuevos cambios

--- build/keys.js
+++ build/keys.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
-        host: "161.35.230.206",
+        host: "164.92.119.121",
         user: "stem",
         password: "gclupita4137",
         database: "bdd_appstem",
--- src/keys.ts
+++ src/keys.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 export default {
   database: {
-    host: "161.35.230.206",
+    host: "164.92.119.121",
     user: "stem",
     password: "gclupita4137",
     database: "bdd_appstem",
